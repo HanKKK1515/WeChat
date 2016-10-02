50c145fe35b93bc2b6bebdd0bc5679017ea08ed3
HanKKK1515 <15814064511@qq.com>
2016-10-02 23:10:21 +0800

    判断设备以及iPad横竖屏，调整登录框的大小，设置输入框和背景图片。

---
+++ WeChat/Classes/Main/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  WeChat
+//
+//  Created by 韩露露 on 16/10/1.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+
+@end
